deb94665c8f31a4f13b42afcc49a61f988334eec
Luong Duy Dat <luongduydat123@gmail.com>
2022-06-22 17:40:44 +0700

    fix to comfort enter_phone and verify screen

--- lib/res/string.dart
+++ lib/res/string.dart
@@ -14,3 +14,8 @@ String signINString = lang=='en'?'Sign in': 'Đăng nhập';
 String passDescription = lang=='en'?'If you need help resetting your password, we can help by sending you a link to reset it.':
     'Nếu bạn muốn đặt lại mật khẩu, chúng tôi có thể gửi link đặt lại mật khẩu cho bạn.';
 String sentString = lang=='en'?'SENT':'GỬI';
+String enterPhoneNumberString = lang=='en'?'To continue enter your phone number':'Để tiếp tục hãy nhập số điện thoại';
+String phoneString = lang=='en'?'Phone':'SĐT';
+String continueString = lang=='en'?'CONTINUE':'TIẾP TỤC';
+String verifyNumberString = lang=='en'?'Verify phone number':'Xác nhận số điện thoại';
+String resentCodeString = lang=='en'?'Resent Code':'Gửi lại mã';
