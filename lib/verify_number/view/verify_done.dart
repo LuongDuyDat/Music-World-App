7ee25ec66c5697e088880df5f45da02a6b5c75e9
Luong Duy Dat <luongduydat123@gmail.com>
2022-06-24 15:11:20 +0700

    add verify number done screen

---
+++ lib/verify_number/view/verify_done.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import '../../components/Button.dart';
+import '../../globals.dart';
+import '../../navigate.dart';
+import '../../util/colors.dart';
+import '../../util/string.dart';
+import '../../util/text_style.dart';
+
+class VerifyDonePage extends StatelessWidget {
+  const VerifyDonePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigate.popPage(context);
+          },
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+              padding: EdgeInsets.only( top: screenHeight * 0.1765),
+              child: Center(
+                child: Text(
+                  verifyDoneString,
+                  style: titleMedium3.copyWith(
+                    color: textPrimaryColor,
+                    height: 1.3,
+                  ),
+                ),
+              )
+
+          ),
+          SizedBox(height: screenHeight * 0.013547,),
+          Center(
+            child: SizedBox(
+              width: screenWidth * 0.4 ,
+              child: Text(
+                haveGoodExperienceString,
+                style: bodyRoboto2.copyWith(
+                  color: textPrimaryColor,
+                  height: 1.3,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            )
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: screenWidth * 0.14267,right: screenWidth * 0.14267, top: screenHeight * 0.1,),
+            child: Button(
+              text: doneString,
+              radius: 0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
